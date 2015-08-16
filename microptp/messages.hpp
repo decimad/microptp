e48b4feded1170f892796ca1a2b17a0c051b116a
Michael Steinberg <mst@tu-clausthal.de>
2015-08-16 14:42:56 +0200

    boost license

--- microptp/messages.hpp
+++ microptp/messages.hpp
@@ -1,3 +1,8 @@
+//          Copyright Michael Steinberg 2015
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE_1_0.txt or copy at
+//          http://www.boost.org/LICENSE_1_0.txt)
+
 #ifndef MICROPTP_MESSAGES_HPP__
 #define MICROPTP_MESSAGES_HPP__
 
